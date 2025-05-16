fa06144e2d2d89da0b06ae3c17470c74cedb73bd
ZhuYifan <zhuyf8899@163.com>
2025-05-16 09:50:16 +0800

    Update main.py

--- google_scholar_crawler/main.py
+++ google_scholar_crawler/main.py
@@ -4,10 +4,10 @@ import json
 from datetime import datetime
 import os
 
-# Setup proxy
-# pg = ProxyGenerator()
-# pg.FreeProxies()  # Use free rotating proxies
-# scholarly.use_proxy(pg)
+Setup proxy
+pg = ProxyGenerator()
+pg.FreeProxies()  # Use free rotating proxies
+scholarly.use_proxy(pg)
 
 author: dict = scholarly.search_author_id(os.environ['GOOGLE_SCHOLAR_ID'])
 scholarly.fill(author, sections=['basics', 'indices', 'counts', 'publications'])
